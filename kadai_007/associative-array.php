5a2592f5b850aef0da8e8c5c664ce979d6449155
sugimoto0925 <hidetora.ryusuke@gmail.com>
2024-08-13 06:10:22 +0900

    Add files via upload

--- kadai_007/associative-array.php
+++ kadai_007/associative-array.php
@@ -12,3 +12,8 @@
         // 連想配列に値を代入する
         $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => '160'];
         print_r($personal_data);
+        ?>
+    </p>
+</body>
+
+</html>
